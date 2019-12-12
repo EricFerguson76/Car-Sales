40c38cbe559794afc04b3062e6d8af55e13b32b8
Eric Ferguson <turtle0888@gmail.com>
2019-12-11 21:15:47 -0600

    Add onClicks

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useDispatch} from 'react-redux';
+import { buy } from '../actions/actions'
+
 
 const AdditionalFeature = props => {
+  const dispatch = useDispatch()
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button" onCLick={() => dispatch(buy(props.feature))}>Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
